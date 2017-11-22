1b49d0d57bcfb58fb67a92d64e57cbe0f83342e5
Yev <evlogosha@gmail.com>
2017-11-22 20:42:50 +0200

    handlebars

--- entries.js
+++ entries.js
@@ -2,72 +2,78 @@ const fs = require('fs');
 
 //add entry
 let addEntry = (id, name, price) => {
-    
     let entryTemplate = {
         id: '',
         name: '',
         price: ''
     }
-    
+
     let newEntry = Object.create(entryTemplate);
     newEntry.id = id;
     newEntry.name = name;
     newEntry.price = price;
-    
+
     let newEntryString = JSON.stringify(newEntry);
-    
-    if(fs.existsSync(`./data/${id}.json`)){
+    //check if file with exact name exists
+    if (fs.existsSync(`./data/${id}.json`)) {
         return console.log(`file ${id} already exists try different name.`)
     }
     fs.writeFile(`./data/${id}.json`, newEntryString, (err) => {
-        if(err) throw err;
+        if (err) throw err;
     });
 
     console.log(`Entry id: ${id} and name: ${name} successfully created`)
-    };
+};
 
-    //delete entry
-    let removeEntry = (id) => {
-        fs.unlink(`./data/${id}.json`, (err) => {
-            if(err) {
-                throw err
-            }
-            console.log(`file ${id} successfully deleted`);
-        })
-    }
-    
-    //edit entry
-    let editEntry = (old_id, new_id, name, price) => {
-        let obj = require(`./data/${old_id}.json`);
-        if(new_id) {
-            obj.id = new_id;
-        } else {
-            obj.id = old_id;
+//delete entry
+let removeEntry = (id) => {
+    fs.unlink(`./data/${id}.json`, (err) => {
+        if (err) {
+            throw err
         }
-        obj.name = name || obj.name;
-        obj.price = price || obj.price;
-        fs.writeFile(`./data/${old_id}.json`, JSON.stringify(obj), (err) => {
-            if(err) throw err;
-        });
-        if(new_id) {
-            fs.rename(`./data/${old_id}.json`, `./data/${new_id}.json`, (err) => {
-                if(err) throw err;
-            })
-            console.log(`File id: ${old_id} renamed to file id: ${new_id}`)
-        }
-        console.log(`File id: ${old_id} has been updated`);
+        console.log(`file ${id} successfully deleted`);
+    })
+}
+
+//edit entry
+let editEntry = (old_id, new_id, name, price) => {
+    let obj = require(`./data/${old_id}.json`);
+    //wether new id has been provided or not
+    if (new_id) {
+        obj.id = new_id;
+    } else {
+        obj.id = old_id;
     }
-    
-    //view Entry
-    let viewEntry = (id) => {
-        fs.readFile(`./data/${id}.json`, 'utf8', (err,data) => {
+
+    obj.name = name || obj.name;
+    obj.price = price || obj.price;
+
+    //update file 
+    fs.writeFile(`./data/${old_id}.json`, JSON.stringify(obj), (err) => {
+        if (err) throw err;
+    });
+    //if new id provided - rename json file
+    if (new_id) {
+        fs.rename(`./data/${old_id}.json`, `./data/${new_id}.json`, (err) => {
             if (err) throw err;
-            console.log(data);
         })
+        console.log(`File id: ${old_id} renamed to file id: ${new_id}`)
     }
+    console.log(`File id: ${old_id} has been updated`);
+}
+
+//view Entry
+let viewEntry = (id) => {
+    fs.readFile(`./data/${id}.json`, 'utf8', (err, data) => {
+        if (err) throw err;
+        console.log(data);
+    })
+}
 
 
-
-
-
-    module.exports = {addEntry, removeEntry, editEntry, viewEntry};
+module.exports = {
+    addEntry,
+    removeEntry,
+    editEntry,
+    viewEntry
+};
--- server.js
+++ server.js
@@ -2,46 +2,17 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');
-
-
-const entries = require('./entries');
-
+const path = require('path');
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Listening on ' + port));
+const hbs = require('express-handlebars');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static(__dirname + '/views'));
+const routes = require('./routes/index');
 
-app.get('/', (req, res) => {
-    res.render('index');
-})
+//app.use(express.static(__dirname + '/public'));
+app.use('/', routes);
 
-app.post('/submit', (req, res) => {
-    let id = req.body.id;
-    let name = req.body.name;
-    let price = req.body.price;
-    entries.addEntry(id, name, price);
-    res.redirect('/')
-})
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts'}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
-app.post('/delete', (req, res) => {
-    let id = req.body.del;
-    entries.removeEntry(id);
-    res.redirect('/');
-})
-
-app.post('/update', (req, res) => {
-    let old_id = req.body.old_id;
-    let new_id = req.body.new_id;
-    let name = req.body.edit_name;
-    let price = req.body.edit_price;
-    entries.editEntry(old_id, new_id, name, price);
-    res.redirect('/');
-})
-
-app.post('/view', (req, res) => {
-    let id = req.body.view;
-    entries.viewEntry(id);
-    res.redirect('/');
-})
+app.listen(port, () => console.log(`Listening on ${port}`));
