9e187ca7ba746ce5ec9676df2ec6a4c06714947e
Yev <evlogosha@gmail.com>
2017-11-23 14:27:31 +0200

    update id added, view entry by id rewritten

--- entries.js
+++ entries.js
@@ -40,24 +40,21 @@ let removeEntry = (id) => {
     let obj = require('./data/data.json');
     let objData = obj.data;
     let allids = objData.map(obj => obj.id);
-// console.log(allids)
-// console.log(objData)
-//get index of requested id and remove from existing object
-function removeEntry(arr, value) {
-    arr.forEach((item) => {
-        if(arr.indexOf(item) === value) {
-            arr.splice(arr.indexOf(item), 1)
-        }
-    })
-   // console.log(arr)
-    return arr;
-}
-removeEntry(objData, allids.indexOf(id))
-//write updated object back to data file
-fs.writeFile('./data/data.json', JSON.stringify(obj, null, 2), (err) => {
-    if(err) throw err;
-});
-console.log(`Entry with id: ${id} has been removed`);
+    //get index of requested id and remove from existing object
+    function removeEntry(arr, value) {
+        arr.forEach((item) => {
+            if (arr.indexOf(item) === value) {
+                arr.splice(arr.indexOf(item), 1)
+            }
+        })
+        return arr;
+    }
+    removeEntry(objData, allids.indexOf(id))
+    //write updated object back to data file
+    fs.writeFile('./data/data.json', JSON.stringify(obj, null, 2), (err) => {
+        if (err) throw err;
+    });
+    console.log(`Entry with id: ${id} has been removed`);
 }
 
 //edit entry
@@ -66,31 +63,45 @@ let editEntry = (old_id, new_id, name, price) => {
     let objData = obj.data;
     let allids = objData.map(obj => obj.id);
 
-//get index of requested id and update from existing object
-function updateEntry(arr, value) {
-    arr.forEach((item) => {
-        if(arr.indexOf(item) === value) {
-            let entry = arr[arr.indexOf(item)]
-            entry.name = name || entry.name;
-            entry.price = price || entry.price;
-            console.log(arr[arr.indexOf(item)])
-        }
-    })
-    return arr;
-}
-updateEntry(objData, allids.indexOf(old_id));
-//write new data
-fs.writeFile('./data/data.json', JSON.stringify(obj, null, 2), (err) => {
-    if(err) throw err;
-});
+    //get index of requested id and update from existing object
+    function updateEntry(arr, value) {
+        arr.forEach((item) => {
+            if (arr.indexOf(item) === value) {
+                let entry = arr[arr.indexOf(item)]
+                //check if new id supplied then update id
+                if(new_id) {
+                    entry.id = new_id;
+                }
+                entry.name = name || entry.name;
+                entry.price = price || entry.price;
+                console.log(arr[arr.indexOf(item)])
+            }
+        })
+        return arr;
+    }
+    updateEntry(objData, allids.indexOf(old_id));
+    //write new data
+    fs.writeFile('./data/data.json', JSON.stringify(obj, null, 2), (err) => {
+        if (err) throw err;
+    });
 }
 
-//view Entry
+//view Entry bi id
 let viewEntry = (id) => {
-    fs.readFile(`./data/${id}.json`, 'utf8', (err, data) => {
-        if (err) throw err;
-        console.log(data);
-    })
+    let obj = require('./data/data.json');
+    let objData = obj.data;
+    let allids = objData.map(obj => obj.id);
+
+    //get index of requested id and display from existing object
+    function viewEntry(arr, value) {
+        arr.forEach((item) => {
+            if (arr.indexOf(item) === value) {
+                console.log(arr[arr.indexOf(item)])
+            }
+        })
+        return arr;
+    }
+    viewEntry(objData, allids.indexOf(id));
 }
 
 
