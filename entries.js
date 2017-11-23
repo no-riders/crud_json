8fe233466b72e4c4a5b2b361248c3ffd2573e6d7
Yev <evlogosha@gmail.com>
2017-11-23 11:59:14 +0200

    add entry rewritten to save entries to 1 file

--- entries.js
+++ entries.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 //add entry
 let addEntry = (id, name, price) => {
     let entryTemplate = {
-        id: '',
-        name: '',
-        price: ''
+        id,
+        name,
+        price
     }
 
     let newEntry = Object.create(entryTemplate);
@@ -14,16 +14,32 @@ let addEntry = (id, name, price) => {
     newEntry.price = price;
 
     let newEntryString = JSON.stringify(newEntry);
-    //check if file with exact name exists
-    if (fs.existsSync(`./data/${id}.json`)) {
-        return console.log(`file ${id} already exists try different name.`)
-    }
-    fs.writeFile(`./data/${id}.json`, newEntryString, (err) => {
-        if (err) throw err;
-    });
 
-    console.log(`Entry id: ${id} and name: ${name} successfully created`)
-};
+    //get data file
+    let obj = require('./data/data.json');
+    //grab array of objects
+    let objData = obj.data;
+    //grab id's from objects, check if id from newly added obj is already inside data file
+    let allids = objData.map(obj => obj.id);
+    let currentObj = JSON.parse(newEntryString);
+
+    if (allids.indexOf(currentObj.id) > -1) {
+        console.log('Error, ID is taken')
+    } else {
+        obj.data.push(currentObj);
+
+        fs.writeFile(`./data/data.json`, JSON.stringify(obj, null, 2), (err) => {
+            if (err) throw err;
+        });
+        console.log(`Entry id: ${id} and name: ${name} successfully created`)
+    };
+}
+
+//let final = JSON.stringify(obj);
+//check if file with exact name exists
+// if (fs.existsSync(`./data/${id}.json`)) {
+//     return console.log(`file ${id} already exists try different name.`)
+// }
 
 //delete entry
 let removeEntry = (id) => {
@@ -49,17 +65,17 @@ let editEntry = (old_id, new_id, name, price) => {
     obj.price = price || obj.price;
 
     //update file 
-    fs.writeFile(`./data/${old_id}.json`, JSON.stringify(obj), (err) => {
+    fs.writeFile(`./data/data.json`, JSON.stringify(obj), (err) => {
         if (err) throw err;
     });
     //if new id provided - rename json file
-    if (new_id) {
-        fs.rename(`./data/${old_id}.json`, `./data/${new_id}.json`, (err) => {
-            if (err) throw err;
-        })
-        console.log(`File id: ${old_id} renamed to file id: ${new_id}`)
-    }
-    console.log(`File id: ${old_id} has been updated`);
+    //     if (new_id) {
+    //         fs.rename(`./data/${old_id}.json`, `./data/${new_id}.json`, (err) => {
+    //             if (err) throw err;
+    //         })
+    //         console.log(`File id: ${old_id} renamed to file id: ${new_id}`)
+    //     }
+    //     console.log(`File id: ${old_id} has been updated`);
 }
 
 //view Entry
