a275c2f7ef380a8cf42ea167257593cc5f029941
Yev <evlogosha@gmail.com>
2017-11-26 13:31:00 +0200

    basic styling added

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const fs = require('fs');
 const path = require('path');
 const port = process.env.PORT || 3000;
 const hbs = require('express-handlebars');
 
 const routes = require('./routes/index');
 
-app.use('/data', express.static(__dirname + '/data'));
-app.use('/', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 app.use('/', routes);
 
 app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts'}));
